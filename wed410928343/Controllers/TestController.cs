6c1da42fa5afac12e9ea0e9f212cd8dc4d38a708
s1092834 <s1092834@pu.edu.tw>
2022-04-28 19:52:58 +0800

    add hw1

---
+++ wed410928343/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wed410928343.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: A
+        public ActionResult A()
+        {
+            return View();
+        }
+        public ActionResult HW1()
+        {
+            return View();
+        }
+    }
+}
